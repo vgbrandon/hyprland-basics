03b04050c1804a4a427357ce49423451979dc2bf
vgbrandon <villegas.galvan.brandon@gmail.com>
2025-09-28 15:12:31 -0500

    first commit

---
+++ postinstall.sh
@@ -0,0 +1,19 @@
+sudo pacman -S --noconfirm \
+  thunar \
+  gvfs \
+  tumbler \
+  ffmpegthumbnailer \
+  file-roller \
+  thunar-archive-plugin \
+  thunar-media-tags-plugin \
+  thunar-shares-plugin \
+  thunar-volman \
+  kitty \
+  firefox \
+  neovim \
+  polkit-gnome \
+  os-prober \
+  ntfs-3g \
+  curl \
+  wget \
+  unzip
